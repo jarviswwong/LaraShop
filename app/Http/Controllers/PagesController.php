b3cac7d4bc9ed2e0085dfdef045b706594ca42a0
imcavoy <yyijian601@gmail.com>
2018-11-17 15:21:35 +0800

    Add email_verified middleware

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,4 +10,8 @@ class PagesController extends Controller
     {
         return view('pages.root');
     }
+
+    public function emailVerifiedNotice(Request $request) {
+        return view('pages.email_verify_notice');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', 'PagesController@root')->name('root');
 
-Auth::routes();
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifiedNotice')->name('email_verify_notice');
+
+    Route::group(['middleware' => 'email_verified'], function (){
+        Route::get('/test', function() {
+            return "Your email is verified";
+        });
+    });
+});

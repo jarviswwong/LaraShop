b69f314059aea7817b221811df84955028c3c631
imcavoy <yyijian601@gmail.com>
2018-12-02 14:32:01 +0800

    Inject 'alipay' and 'wechat_pay' into the App container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use function foo\func;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,5 +30,30 @@ class AppServiceProvider extends ServiceProvider
             // IDE提示插件
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
+
+        // 注入Alipay对象到容器中
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+            if (app()->environment() !== 'production') {
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 创建一个支付宝支付对象
+            return Pay::alipay($config);
+        });
+
+        // 注入Wechat_pay到容器中
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 创建一个微信支付对象
+            return Pay::wechat($config);
+        });
     }
 }

a1ae470cddc5230faf5f77a411220588ffda59c3
xuanxiao <yyijian601@gmail.com>
2020-03-15 14:03:01 +0800

    使用延迟队列发送订单支付邮件

--- app/Notifications/OrderPaidNotification.php
+++ app/Notifications/OrderPaidNotification.php
@@ -4,11 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Order;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class OrderPaidNotification extends Notification
+class OrderPaidNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
